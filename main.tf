c94a204f8360cb36567c88ca0889aae77396aacd
Tinajero, Jaime Daniel (LNG-MEX) <jaimedaniel.tinajero+lncp@lexisnexis.com>
2025-07-30 15:59:01 -0600

    UPDATING SUBNET BLOCK

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "aws_vpc" "selected_vpc" {
 
 resource "aws_subnet" "default_subnet" {
   vpc_id                  = data.aws_vpc.selected_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
 
